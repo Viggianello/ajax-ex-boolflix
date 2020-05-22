e831b27622c8e7e605e0499736ba05696521d323
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-05-22 16:49:35 +0200

    working progress funzione

--- main.js
+++ main.js
@@ -14,7 +14,8 @@
 // Come vi dicevo, per il momento non è importante l'aspetto grafico: i risultati possono essere inseriti in pagina come semplici ul, anche senza handlebars.
 
 $(document).ready(function() {
-    var ricerca = 'batman';
+    // var ricerca = RicercaUtente();
+    var ricerca = 'Batman';
 	//Chiamata ajax
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
@@ -37,55 +38,57 @@ $(document).ready(function() {
     }// fine oggetto
     );
 
-    function stampahtml(infodischi) {
-        var schedadisco = $('#entry-template').html();
-        var template_function = Handlebars.compile(schedadisco);
-
-        for (var i = 0; i < infodischi.length; i++) {
-            var info= infodischi[i];
-            var disco = {
-                'poster': info.poster,
-                'title' : info.title,
-                'author': info.author,
-                'year': info.year,
-                'genre': info.genre,
-                'classe': info.genre,
-            }
-            var html_finale = template_function(disco);
-            $('.cds-container.container').append(html_finale);
-        }
-}
+    // function stampahtml(infodischi) {
+    //     var schedadisco = $('#entry-template').html();
+    //     var template_function = Handlebars.compile(schedadisco);
+    //
+    //     for (var i = 0; i < infodischi.length; i++) {
+    //         var info= infodischi[i];
+    //         var disco = {
+    //             'poster': info.poster,
+    //             'title' : info.title,
+    //             'author': info.author,
+    //             'year': info.year,
+    //             'genre': info.genre,
+    //             'classe': info.genre,
+    //         }
+    //         var html_finale = template_function(disco);
+    //         $('.cds-container.container').append(html_finale);
+    //     }
+// }
  // controllo l'input a sx a ogni tasto digitato (tranne canc e back-space se usassi keypress)
- $('.left .input-container').keyup(function(event){
+ $('#testo-ricerca').keyup(function RicercaUtente(event){
      // recupero il testo dell utente e tiro via gli spazi inutili e lo rendo tutto minuscolo per un confronto ,indipendentemente dal fatto che sia maiuscolo e minuscolo, delle sole lettere
-     var testo_utente = $('.left .input-container input').val().trim().toLowerCase();
+     var testo_utente = $('#testo-ricerca').val().trim().toLowerCase();
+     return testo_utente;
+     console.log(testo_utente);
      // stampo ciò che leggo
      // console.log('testo utente:' + testo_utente);
      // controllo che l'utente non ha digitato nulla(ha riempito l'input)
-     if (testo_utente != '') {
-         // all interno di liste-chat prendo ogni h1
-         $('.cds-container .cd').each(function() {
-         // recupero il testo di questo h1 e lo rendo tutto minuscolo
-         var testo_genere = $(this).find('.genre').text().trim().toLowerCase();
-         // stampo il testo di ogni h1
-         console.log('testo h1:' + testo_genere);
+     // if (testo_utente != '') {
+     //     // all interno di liste-chat prendo ogni h1
+     //     $('.cds-container .cd').each(function() {
+     //     // recupero il testo di questo h1 e lo rendo tutto minuscolo
+     //     var testo_genere = $(this).find('.genre').text().trim().toLowerCase();
+     //     // stampo il testo di ogni h1
+     //     console.log('testo h1:' + testo_genere);
          // verifico se è uguale a quello inserito
-         if (testo_genere.includes(testo_utente)) {
-             // allora mostro solo lui(h1)
-             $(this).show();
-             // stampo il testo inserito dall'utente se  è contenuto id uno dei testi presenti negli h1
-             console.log('la digitazione inserita è inclusa nel nome sopra:' + testo_utente);
-         }
-         else {
-             // allora nascondo il resto delle .riga
-             $(this).hide();
-         }
-         })// chiudo l'each
-     }// chiudo l'if controllo input diverso da stringa vuota
-     else {// allora l'input è vuoto e rimostro tutte le .riga
-         $('.cd').show();
-     }
+     //     if (testo_genere.includes(testo_utente)) {
+     //         // allora mostro solo lui(h1)
+     //         $(this).show();
+     //         // stampo il testo inserito dall'utente se  è contenuto id uno dei testi presenti negli h1
+     //         console.log('la digitazione inserita è inclusa nel nome sopra:' + testo_utente);
+     //     }
+     //     else {
+     //         // allora nascondo il resto delle .riga
+     //         $(this).hide();
+     //     }
+     //     })// chiudo l'each
+     // }// chiudo l'if controllo input diverso da stringa vuota
+     // else {// allora l'input è vuoto e rimostro tutte le .riga
+     //     $('.cd').show();
+     // }
      // }   // chiudo l'if del 13
- });// chiudo il keyup
+    });// chiudo il keyup
 
 });
