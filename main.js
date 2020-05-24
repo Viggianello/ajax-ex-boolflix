8c45a3222c8b85969772e13f9e8c416c79b74bba
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-05-24 13:23:10 +0200

    inserito il paragrafo descrittivo del film

--- main.js
+++ main.js
@@ -14,7 +14,7 @@
 // Come vi dicevo, per il momento non è importante l'aspetto grafico: i risultati possono essere inseriti in pagina come semplici ul, anche senza handlebars.
 
 $(document).ready(function() {
-    // var ricerca = RicercaUtente();
+    // Creo una funzione legata al invio inserito nell'input in modo tale da far partire la ricerca senza dover cliccare sul pulsante cerca
     $('#testo-ricerca').keypress().keypress(function(event){
         var keycode = (event.keyCode ? event.keyCode : event.which);
         // codice tasto invio
@@ -25,7 +25,7 @@ $(document).ready(function() {
         }// chiudo l'if controllo input inserito da tastiera tasto 13 ossia invio
     });// chiudo il keypress dell'input
 
-    // pulsante cerca funzione al suo click mostra i risultati della chiamata ajax
+    // Creo una funzione legata al click del pulsante mostranodo i risultati della chiamata ajax
     $('#pulsante-ricerca').click(function() {
         // resetto l input da precedenti ricerche
         $('#risultati').html('');
@@ -33,84 +33,64 @@ $(document).ready(function() {
         chiamataAjax(valore);
     })
 
-function chiamataAjax(valore) {
-    //Chiamata ajax
-    $.ajax({
-        'url': 'https://api.themoviedb.org/3/search/movie',
-        'method': 'GET',
-        'data': {
-            'api_key': '4a0b8c67695163b99de0216fcb0bfb27',
-            'query': valore,
-            'language': 'it'
-        },
-        'success': function(risposta) {
-        // var infodisco = riposta.response;
-            // stampo le informazioni per ogni disco
-            console.log(risposta);
-            // salvo i risultati
-            var risultati = risposta.results;
-            for (var i = 0; i < risultati.length; i++) {
-                // leggo per ogni risultato 1)titolo 2)titolo originale 3)lingua 4)voto
-                var risultatoCorrente = risultati[i];
-                var titolo = risultatoCorrente.title;
-                var titoloOriginale = risultatoCorrente.original_title;
-                var lingua = risultatoCorrente.original_language;
-                var voto = risultatoCorrente.vote_average;
-                var copertina = 'http://image.tmdb.org/t/p/w342/' +     risultatoCorrente.backdrop_path;
-                var img = document.createElement("img");
-                img.src = copertina;
-                console.log(copertina);
-                // stampo i risultati ottenuti
-                // console.log('il titolo è: ' + titolo);
-                // console.log('il titolo originale è: ' + titoloOriginale);
-                // console.log('la lingua è: ' + lingua);
-                // console.log('il voto è: ' + voto);
-                $('#risultati').append('<li>' + 'il titolo è: ' + titolo + '</li>');
-                if (titolo != titoloOriginale ) {
-                    $('#risultati').append('<li>' + 'il titolo originale è: ' + titoloOriginale + '</li>');
-                }
-                $('#risultati').append('<li>' + 'la lingua è: ' + lingua + '</li>');
-                $('#risultati').append('<li>' + 'il voto è: ' + voto + '</li>');
-                if (risultatoCorrente.backdrop_path != null) {
-                    // se ci sono le immagini allora metto il tag img con src che è diverso da nulla(ossia esiste) e visualizzo tale immagine in pagina
-                    $('#risultati').append(img);
-                }
-            }
+    // Creo una funzione per la chiamata ajax legata al parametro valore che sarà quello che leggeremo dall'input che metterà l'utente
+    function chiamataAjax(valore) {
+        //Chiamata ajax
+        $.ajax({
+            'url': 'https://api.themoviedb.org/3/search/movie',
+            'method': 'GET',
+            'data': {
+                'api_key': '4a0b8c67695163b99de0216fcb0bfb27',
+                'query': valore,
+                'language': 'it'
+            },
+            'success': function(risposta) {
+            // var infodisco = riposta.response;
+                // stampo le informazioni per ogni disco
+                console.log(risposta);
+                // salvo i risultati
+                var risultati = risposta.results;
+                for (var i = 0; i < risultati.length; i++) {
+                    // leggo per ogni risultato 1)titolo 2)titolo originale 3)lingua 4)voto 5)l'immagine copertina 6)la descrizione del film
+                    var risultatoCorrente = risultati[i];
+                    var titolo = risultatoCorrente.title;
+                    var titoloOriginale = risultatoCorrente.original_title;
+                    var lingua = risultatoCorrente.original_language;
+                    var voto = risultatoCorrente.vote_average;
+                    // http://image.tmdb.org/t/p/w342/3lUM7vYmKnse9qO7eYwZfhRiDVy.jpg
+                    // ossia src completo di una immagine dove varia solo la parte dopo w342
+                    var copertina = 'http://image.tmdb.org/t/p/w342/' +     risultatoCorrente.backdrop_path;
+                    var img = document.createElement("img");
+                    img.src = copertina;
+                    // recupero il testo che descrive il film
+                    var descrizione = risultatoCorrente.overview;
+                    // metto in pagina i dati estrapolati dalla URL
+                    $('#risultati').append('<li>' + 'il titolo è: ' + titolo + '</li>');
+                    if (titolo != titoloOriginale ) {
+                        $('#risultati').append('<li>' + 'il titolo originale è: ' + titoloOriginale + '</li>');
+                    }
+                    $('#risultati').append('<li>' + 'la lingua è: ' + lingua + '</li>');
+                    $('#risultati').append('<li>' + 'il voto è: ' + voto + '</li>');
+                    if (risultatoCorrente.backdrop_path != null) {
+                        // se ci sono le immagini allora metto il tag img con src che è diverso da nulla(ossia esiste) e visualizzo tale immagine in pagina
+                        $('#risultati').append(img);
+                    }
+                    $('#risultati').append('<p class="descrizioneFilm">'+ descrizione +'</p>');
 
-            // stampahtml(infodisco);
-            // selectgeneri(infodisco);
-        },
-        'error':function(){
-            alert('errore');
-        }
-    }// fine oggetto
-    );
-}
-function ricercaUtente(){
-    // recupero il testo dell utente (inserito nell input)e tiro via gli spazi inutili e lo rendo tutto minuscolo per un confronto ,indipendentemente dal fatto che sia maiuscolo e minuscolo, delle sole lettere
-    var testo_utente = $('#testo-ricerca').val().trim().toLowerCase();
-    return testo_utente;
-};// chiudo il keyup
+                }// chiusura ciclo for
+
+            },
+            'error':function(){
+                alert('errore');
+            }
+        }// fine oggetto
+        );
+    }// finefunzione chiamataAjax
+
+    function ricercaUtente(){
+        // recupero il testo dell utente (inserito nell input), tiro via gli spazi inutili e lo rendo tutto minuscolo (per un confronto migliore)
+        var testo_utente = $('#testo-ricerca').val().trim().toLowerCase();
+        return testo_utente;
+    }
 
 });
-
-
-// function stampahtml(infodischi) {
-//     var schedadisco = $('#entry-template').html();
-//     var template_function = Handlebars.compile(schedadisco);
-//
-//     for (var i = 0; i < infodischi.length; i++) {
-//         var info= infodischi[i];
-//         var disco = {
-//             'poster': info.poster,
-//             'title' : info.title,
-//             'author': info.author,
-//             'year': info.year,
-//             'genre': info.genre,
-//             'classe': info.genre,
-//         }
-//         var html_finale = template_function(disco);
-//         $('.cds-container.container').append(html_finale);
-//     }
-// }
-// controllo l'input a sx a ogni tasto digitato (tranne canc e back-space se usassi keypress)
